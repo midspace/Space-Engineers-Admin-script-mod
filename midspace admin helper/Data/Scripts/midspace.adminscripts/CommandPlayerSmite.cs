56a60bfef5b0112c7bd5ee5403df94e9cd7d748b
midspace <midspace@hotmail.com>
2015-02-20 10:49:33 +0800

    Smite changes
    changing vectors on Smite.
    Initial vector appears to come from chest up towards the head, and added
    global for max speed.
    I have flipped it to come down on the head, and retested in DS with
    single players. Needs more testing with multiple players.

--- midspace admin helper/Data/Scripts/midspace.adminscripts/CommandPlayerSmite.cs
+++ midspace admin helper/Data/Scripts/midspace.adminscripts/CommandPlayerSmite.cs
@@ -7,6 +7,7 @@
 
     using Sandbox.ModAPI;
     using Sandbox.Common.ObjectBuilders;
+    using Sandbox.Definitions;
 
     public class CommandPlayerSmite : ChatCommand
     {
@@ -56,17 +57,19 @@
                 MyAPIGateway.Utilities.ShowMessage("smiting", selectedPlayer.DisplayName);
                 var worldMatrix = selectedPlayer.Controller.ControlledEntity.GetHeadMatrix(true, true, true);
 
+                var maxspeed = MyDefinitionManager.Static.EnvironmentDefinition.SmallShipMaxSpeed * 1.25f;
+
                 var meteor = new MyObjectBuilder_Meteor
                 {
                     Item = new MyObjectBuilder_InventoryItem { Amount = 1, Content = new MyObjectBuilder_Ore { SubtypeName = _oreNames[0] } },
                     PersistentFlags = MyPersistentEntityFlags2.InScene, // Very important
                     PositionAndOrientation = new MyPositionAndOrientation
                     {
-                        Position = (worldMatrix.Translation + worldMatrix.Up * 0.05f).ToSerializableVector3D(),
+                        Position = (worldMatrix.Translation + worldMatrix.Up * -0.5f).ToSerializableVector3D(),
                         Forward = worldMatrix.Forward.ToSerializableVector3(),
                         Up = worldMatrix.Up.ToSerializableVector3(),
                     },
-                    LinearVelocity = worldMatrix.Up * 200, // has to be faster than JetPack speed, otherwise it could be avoided.
+                    LinearVelocity = worldMatrix.Down * -maxspeed, // has to be faster than JetPack speed, otherwise it could be avoided.
                     // Update 01.052 seemed to have flipped the direction. It's Up instead of Down???
                     Integrity = 1
                 };
