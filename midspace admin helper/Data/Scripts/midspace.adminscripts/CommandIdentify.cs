1521aa111ffd444a7e82bb70bb91c01c3a94e869
midspace <midspace@hotmail.com>
2015-02-20 14:45:49 +0800

    Fixed #30
    Have broken up the asteroid reading into chunks.
    The LOD 1 is not accurate enough for asteroid details, so have reverted
    it to LOD 0, and have put in Parallel threading to process large
    asteroids.
    Have added asteroid content size also, and rounded centre position
    values.

--- midspace admin helper/Data/Scripts/midspace.adminscripts/CommandIdentify.cs
+++ midspace admin helper/Data/Scripts/midspace.adminscripts/CommandIdentify.cs
@@ -40,31 +40,18 @@
 
                         //float GetVoxelContentInBoundingBox(BoundingBoxD worldAabb, out float cellCount);  // Do we bother with this detail?
 
-                        // TODO: try using IMyParallelTask to process.
+                        if (voxelMap.Storage.Size.RectangularLength() >= 1536) // 512x512x512 or bigger.
+                        {
+                            MyAPIGateway.Utilities.ShowMessage("Asteroid", "{0} is too big to load immediately, so processing is occurring in the background. Details will appear when finished.", displayName);
 
-                        var oldCache = new MyStorageDataCache();
-                        oldCache.Resize(voxelMap.Storage.Size);
-                        voxelMap.Storage.ReadRange(oldCache, MyStorageDataTypeFlags.ContentAndMaterial, 1, Vector3I.Zero, voxelMap.Storage.Size - 1);
-
-                        Vector3I min = Vector3I.MaxValue;
-                        Vector3I max = Vector3I.MinValue;
-
-                        Vector3I p;
-                        for (p.Z = 0; p.Z < voxelMap.Storage.Size.Z; ++p.Z)
-                            for (p.Y = 0; p.Y < voxelMap.Storage.Size.Y; ++p.Y)
-                                for (p.X = 0; p.X < voxelMap.Storage.Size.X; ++p.X)
-                                {
-                                    var content = oldCache.Content(ref p);
-                                    if (content > 0)
-                                    {
-                                        min = Vector3I.Min(min, p);
-                                        max = Vector3I.Max(max, p + 1);
-                                    }
-                                }
-
-                        var contentBox = new BoundingBoxD(voxelMap.PositionLeftBottomCorner + min, voxelMap.PositionLeftBottomCorner + max);
-                        description = string.Format("Distance: {0:N}\r\nSize: {1}\r\nBoundingBox Center: {2}\r\nLOD1 Content Center: {3}", distance, voxelMap.Storage.Size, aabb.Center, contentBox.Center);
-
+                            // use IMyParallelTask to process in the background.
+                            MyAPIGateway.Parallel.StartBackground(
+                                delegate() { ProcessAsteroid(displayType, displayName, voxelMap, distance, aabb); });
+                        }
+                        else
+                        {
+                            ProcessAsteroid(displayType, displayName, voxelMap, distance, aabb);
+                        }
                     }
                     else if (entity is IMyCubeGrid)
                     {
@@ -79,24 +66,25 @@
                         displayType = gridCube.IsStatic ? "Station" : gridCube.GridSizeEnum.ToString() + " Ship";
                         displayName = entity.DisplayName;
                         description = string.Format("Distance: {0:N} m\r\nMass: {1:N} kg\r\nVector: {2}\r\nVelocity: {3:N} m/s\r\nMass Center: {4}\r\nSize: {5}\r\nNumber of Blocks: {6:#,##0}\r\nAttached Grids: {7:#,##0}",
-                            distance, 
-                            gridCube.Physics.Mass, 
-                            gridCube.Physics.LinearVelocity, 
+                            distance,
+                            gridCube.Physics.Mass,
+                            gridCube.Physics.LinearVelocity,
                             gridCube.Physics.LinearVelocity.Length(),
                             gridCube.Physics.CenterOfMassWorld,
                             gridCube.LocalAABB.Size,
                             blocks.Count,
                             attachedGrids.Count
                             );
+                        MyAPIGateway.Utilities.ShowMissionScreen(string.Format("ID {0}:", displayType), string.Format("'{0}'", displayName), " ", description, null, "OK");
                     }
                     else
                     {
                         displayType = "player";
                         displayName = entity.DisplayName;
                         description = string.Format("Distance: {0:N}", distance);
+                        MyAPIGateway.Utilities.ShowMissionScreen(string.Format("ID {0}:", displayType), string.Format("'{0}'", displayName), " ", description, null, "OK");
                     }
 
-                    MyAPIGateway.Utilities.ShowMissionScreen(string.Format("ID {0}:", displayType), string.Format("'{0}'", displayName), " ", description, null, "OK");
                     return true;
                 }
 
@@ -105,6 +93,46 @@
             }
 
             return false;
+        }
+
+        private void ProcessAsteroid(string displayType, string displayName, IMyVoxelMap voxelMap, double distance, BoundingBoxD aabb)
+        {
+            Vector3I min = Vector3I.MaxValue;
+            Vector3I max = Vector3I.MinValue;
+            Vector3I block;
+
+            // read the asteroid in chunks of 64 to avoid the Arithmetic overflow issue.
+            for (block.Z = 0; block.Z < voxelMap.Storage.Size.Z; block.Z += 64)
+                for (block.Y = 0; block.Y < voxelMap.Storage.Size.Y; block.Y += 64)
+                    for (block.X = 0; block.X < voxelMap.Storage.Size.X; block.X += 64)
+                    {
+                        var size = new Vector3I(64);
+                        var oldCache = new MyStorageDataCache();
+                        oldCache.Resize(size);
+                        // LOD1 is not detailed enough for content information on asteroids.
+                        voxelMap.Storage.ReadRange(oldCache, MyStorageDataTypeFlags.Content, 0, block, block + size - 1);
+
+                        Vector3I p;
+                        for (p.Z = 0; p.Z < size.Z; ++p.Z)
+                            for (p.Y = 0; p.Y < size.Y; ++p.Y)
+                                for (p.X = 0; p.X < size.X; ++p.X)
+                                {
+                                    var content = oldCache.Content(ref p);
+                                    if (content > 0)
+                                    {
+                                        min = Vector3I.Min(min, p + block);
+                                        max = Vector3I.Max(max, p + block + 1);
+                                    }
+                                }
+                    }
+
+            var contentBox = new BoundingBoxD(voxelMap.PositionLeftBottomCorner + min, voxelMap.PositionLeftBottomCorner + max);
+            var description = string.Format("Distance: {0:N}\r\nSize: {1}\r\nBoundingBox Center: [X:{2:N} Y:{3:N} Z:{4:N}]\r\n\r\nContent Size:{5}\r\nLOD0 Content Center: [X:{6:N} Y:{7:N} Z:{8:N}]",
+                distance, voxelMap.Storage.Size,
+                aabb.Center.X, aabb.Center.Y, aabb.Center.Z,
+                max - min,
+                contentBox.Center.X, contentBox.Center.Y, contentBox.Center.Z);
+            MyAPIGateway.Utilities.ShowMissionScreen(string.Format("ID {0}:", displayType), string.Format("'{0}'", displayName), " ", description, null, "OK");
         }
     }
 }
