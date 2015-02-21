1ffd6c6b4244b3202a1b7be1478804b18127a9e2
midspace <midspace@hotmail.com>
2015-02-21 18:21:15 +0800

    Fixed #4
    Have rewritten the eject command to remove the use of Identity, as it
    doesn't provide information on Active players, unlike Players.
    And queued the double eject for remoted ships.
    Moved the Bot specified code to the bottom and commented out until the
    API provides functionality to match.

--- midspace admin helper/Data/Scripts/midspace.adminscripts/CommandPlayerEject.cs
+++ midspace admin helper/Data/Scripts/midspace.adminscripts/CommandPlayerEject.cs
@@ -9,6 +9,8 @@
 
     public class CommandPlayerEject : ChatCommand
     {
+        private Queue<Action> _workQueue = new Queue<Action>();
+
         public CommandPlayerEject()
             : base(ChatCommandSecurity.Admin, "eject", new[] { "/eject" })
         {
@@ -22,56 +24,63 @@
         public override bool Invoke(string messageText)
         {
             var match = Regex.Match(messageText, @"/eject\s{1,}(?<Key>.+)", RegexOptions.IgnoreCase);
-
             if (match.Success)
             {
                 var playerName = match.Groups["Key"].Value;
                 var players = new List<IMyPlayer>();
                 MyAPIGateway.Players.GetPlayers(players, p => p != null);
-                IMyIdentity selectedPlayer = null;
+                IMyPlayer selectedPlayer = null;
 
-                var identities = new List<IMyIdentity>();
-                MyAPIGateway.Players.GetAllIdentites(identities, delegate(IMyIdentity i) { return i.DisplayName.Equals(playerName, StringComparison.InvariantCultureIgnoreCase); });
-                selectedPlayer = identities.FirstOrDefault();
+                var findPlayer = players.FirstOrDefault(p => p.DisplayName.Equals(playerName, StringComparison.InvariantCultureIgnoreCase));
+                if (findPlayer != null)
+                {
+                    selectedPlayer = findPlayer;
+                }
 
                 int index;
                 if (playerName.Substring(0, 1) == "#" && Int32.TryParse(playerName.Substring(1), out index) && index > 0 && index <= CommandPlayerStatus.IdentityCache.Count)
                 {
-                    selectedPlayer = CommandPlayerStatus.IdentityCache[index - 1];
-                }
-
-                if (playerName.Substring(0, 1) == "B" && Int32.TryParse(playerName.Substring(1), out index) && index > 0 && index <= CommandListBots.BotCache.Count)
-                {
-                    selectedPlayer = CommandListBots.BotCache[index - 1];
+                    var listplayers = new List<IMyPlayer>();
+                    MyAPIGateway.Players.GetPlayers(listplayers, p => p.PlayerID == CommandPlayerStatus.IdentityCache[index - 1].PlayerId);
+                    selectedPlayer = listplayers.FirstOrDefault();
                 }
 
                 if (selectedPlayer == null)
-                    return false;
-
-                var listplayers = new List<IMyPlayer>();
-                MyAPIGateway.Players.GetPlayers(listplayers, p => p.PlayerID == selectedPlayer.PlayerId);
-                var player = listplayers.FirstOrDefault();
-
-                if (player != null)
                 {
-                    if (player.Controller.ControlledEntity.Entity.Parent != null)
-                    {
-                        MyAPIGateway.Utilities.ShowMessage("ejecting", player.DisplayName);
-                        player.Controller.ControlledEntity.Use();
-
-                        // Neither of these do what I expect them to. In fact, I'm not sure what they do.
-                        //MyAPIGateway.Players.RemoveControlledEntity(player.Controller.ControlledEntity.Entity);
-                        //MyAPIGateway.Players.RemoveControlledEntity(player.Controller.ControlledEntity.Entity.Parent);
-                    }
-                    else
-                    {
-                        MyAPIGateway.Utilities.ShowMessage("player", string.Format("{0} is not a pilot", player.DisplayName));
-                    }
-
+                    MyAPIGateway.Utilities.ShowMessage("Eject", string.Format("No player named {0} found.", playerName));
                     return true;
                 }
 
-                // selectedPlayer is a Bot? 
+                if (selectedPlayer.Controller.ControlledEntity.Entity.Parent != null)
+                {
+                    MyAPIGateway.Utilities.ShowMessage("ejecting", selectedPlayer.DisplayName);
+                    selectedPlayer.Controller.ControlledEntity.Use();
+
+                    // Enqueue the command a second time, to make sure the player is ejected from a remote controlled ship and a piloted ship.
+                    _workQueue.Enqueue(delegate() {
+                        if (selectedPlayer.Controller.ControlledEntity.Entity.Parent != null)
+                        {
+                            selectedPlayer.Controller.ControlledEntity.Use();
+                        }
+                    });
+
+                    // Neither of these do what I expect them to. In fact, I'm not sure what they do.
+                    //MyAPIGateway.Players.RemoveControlledEntity(player.Controller.ControlledEntity.Entity);
+                    //MyAPIGateway.Players.RemoveControlledEntity(player.Controller.ControlledEntity.Entity.Parent);
+                }
+                else
+                {
+                    MyAPIGateway.Utilities.ShowMessage("player", string.Format("{0} is not a pilot", selectedPlayer.DisplayName));
+                }
+                return true;
+
+                // NPC's do not appears as Players, but Identities.
+                // There could be multiple Identities with the same name, for active, inactive and dead.
+                //if (playerName.Substring(0, 1) == "B" && Int32.TryParse(playerName.Substring(1), out index) && index > 0 && index <= CommandListBots.BotCache.Count)
+                //{
+                //    selectedPlayer = CommandListBots.BotCache[index - 1];
+                //}
+
                 // TODO: figure out how to eject Autopilot.
 
                 //var entities = new HashSet<IMyEntity>();
@@ -95,6 +104,16 @@
             }
 
             return false;
+        }
+
+
+        public override void UpdateBeforeSimulation100()
+        {
+            if (_workQueue.Count > 0)
+            {
+                var action = _workQueue.Dequeue();
+                action.Invoke();
+            }
         }
     }
 }
