9e75aa82137efcadc9af3222bfd244cf701c8ddb
midspace <midspace@hotmail.com>
2015-10-24 12:08:07 +1100

    New message classes
    These are for sending text message back to the player from the server.

---
+++ midspace admin helper/Data/Scripts/midspace.adminscripts/Messages/Communication/MessageClientDialogMessage.cs
@@ -0,0 +1,33 @@
+﻿namespace midspace.adminscripts.Messages.Communication
+{
+    using ProtoBuf;
+    using Sandbox.ModAPI;
+
+    [ProtoContract]
+    public class MessageClientDialogMessage : MessageBase
+    {
+        [ProtoMember(1)]
+        public string Title;
+
+        [ProtoMember(2)]
+        public string Prefix;
+
+        [ProtoMember(3)]
+        public string Content;
+
+        public override void ProcessClient()
+        {
+            MyAPIGateway.Utilities.ShowMissionScreen(Title, Prefix, " ", Content);
+        }
+
+        public override void ProcessServer()
+        {
+            // never processed on server.
+        }
+
+        public static void SendMessage(ulong steamId, string title, string prefix, string content)
+        {
+            ConnectionHelper.SendMessageToPlayer(steamId, new MessageClientDialogMessage { Title = title, Prefix = prefix, Content = content });
+        }
+    }
+}
---
+++ midspace admin helper/Data/Scripts/midspace.adminscripts/Messages/Communication/MessageClientTextMessage.cs
@@ -0,0 +1,36 @@
+﻿namespace midspace.adminscripts.Messages.Communication
+{
+    using ProtoBuf;
+    using Sandbox.ModAPI;
+
+    [ProtoContract]
+    public class MessageClientTextMessage : MessageBase
+    {
+        [ProtoMember(1)]
+        public string Prefix;
+
+        [ProtoMember(2)]
+        public string Content;
+
+        public override void ProcessClient()
+        {
+            MyAPIGateway.Utilities.ShowMessage(Prefix, Content);
+        }
+
+        public override void ProcessServer()
+        {
+            // never processed on server.
+        }
+
+        public static void SendMessage(ulong steamId, string prefix, string content, params object[] args )
+        {
+            string message;
+            if (args == null || args.Length == 0)
+                message = content;
+            else
+                message = string.Format(content, args);
+
+            ConnectionHelper.SendMessageToPlayer(steamId, new MessageClientTextMessage { Prefix = prefix, Content = message });
+        }
+    }
+}
