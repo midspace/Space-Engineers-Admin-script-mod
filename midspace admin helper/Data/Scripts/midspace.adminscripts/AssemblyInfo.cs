c9eac4ec581ccdb9750e4d5de3ac79436d2b6ea0
midspace <midspace@hotmail.com>
2015-11-01 12:49:07 +1100

    minor updates
    gramma, and updates.

--- midspace admin helper/Data/Scripts/midspace.adminscripts/AssemblyInfo.cs
+++ midspace admin helper/Data/Scripts/midspace.adminscripts/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Reflection;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Mid-Space Productions")]
 [assembly: AssemblyProduct("Space Engineers")]
-[assembly: AssemblyCopyright("Copyright © MidSpace 2014")]
+[assembly: AssemblyCopyright("Copyright © MidSpace 2014-2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -15,7 +15,7 @@ using System.Reflection;
 // The AssemblyInfo isn't promoted by KeenSWH to be included in their 'script' mods.
 // So it may not work in the future.
 // Currently is does work, and it provides identifiable information in the compiled runtime 
-// if any ever attempt to decompile it directly.
+// if anyone ever attempts to decompile it directly.
 
 // I do not recommend changing the version. This will probably cause runtime issue if the version number
 // is changed after Space Engineers is loaded. Set it at 1.0.0.0, and leave it like that.
