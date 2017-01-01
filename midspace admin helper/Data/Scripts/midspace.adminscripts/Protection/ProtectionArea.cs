964f9e1c8a8bce4a6b8120ab16e1d4a90b64df27
midspace <midspace@internode.on.net>
2017-01-01 12:05:04 +1100

    #174 Fix Protection area performance issues Change to how non FatBlock world locations are calculated, using an interal API which from very basic testing should be faster.

--- midspace admin helper/Data/Scripts/midspace.adminscripts/Protection/ProtectionArea.cs
+++ midspace admin helper/Data/Scripts/midspace.adminscripts/Protection/ProtectionArea.cs
@@ -60,43 +60,17 @@
             if (block.FatBlock != null)
                 return Contains(block.FatBlock);
 
-            // since some blocks aren't an entity we have to deal with them otherwise
-            // we'll create an grid that acts as substitute for the block to get an entity that has a collision
-            // TODO find a better suited way to get the boundings of an armor block
-            var worldPosition = block.CubeGrid.GetPosition() + block.Position;
+            BoundingBoxD box;
+            block.GetWorldBoundingBox(out box);
 
-            // creating grid
-            var gridBuilder = new MyObjectBuilder_CubeGrid()
+            switch (Shape)
             {
-                PersistentFlags = MyPersistentEntityFlags2.None,
-                PositionAndOrientation = new MyPositionAndOrientation(worldPosition, Vector3.Forward, Vector3.Up),
-                DisplayName = "substitute",
-                GridSizeEnum = block.CubeGrid.GridSizeEnum
-            };
-
-            // creating cube block
-            MyObjectBuilder_CubeBlock cube = new MyObjectBuilder_CubeBlock();
-            cube.Min = block.Position;
-            cube.SubtypeName = block.CubeGrid.GridSizeEnum == MyCubeSize.Large ? "LargeBlockArmorBlock" : "SmallBlockArmorBlock";
-            cube.BuildPercent = 0;
-            gridBuilder.CubeBlocks.Add(cube);
-
-            // add grid
-            MyAPIGateway.Entities.RemapObjectBuilder(gridBuilder);
-            var entity = MyAPIGateway.Entities.CreateFromObjectBuilder(gridBuilder);
-            var grid = entity as IMyCubeGrid;
-            var blocks = new List<IMySlimBlock>();
-
-            // get blocks
-            grid.GetBlocks(blocks, b => b != null);
-
-            if (blocks.Count > 0)
-            {
-                // found block
-                var cubeBlock = blocks[0].FatBlock;
-                bool isInside = Contains(cubeBlock);
-                grid.Close();
-                return isInside;
+                case ProtectionAreaShape.Cube:
+                    var boundingBox = new BoundingBoxD(new Vector3D(Center.X - Size, Center.Y - Size, Center.Z - Size), new Vector3D(Center.X + Size, Center.Y + Size, Center.Z + Size));
+                    return boundingBox.Intersects(box);
+                case ProtectionAreaShape.Sphere:
+                    var boundingSphere = new BoundingSphereD(Center, Size);
+                    return boundingSphere.Intersects(box);
             }
 
             return false;
