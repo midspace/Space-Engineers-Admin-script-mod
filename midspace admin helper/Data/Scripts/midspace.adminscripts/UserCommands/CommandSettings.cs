d3592c2f88b927cf83714c7855c1e6550d57b515
midspace <midspace@hotmail.com>
2017-12-07 18:35:07 +1100

    Update.

--- midspace admin helper/Data/Scripts/midspace.adminscripts/UserCommands/CommandSettings.cs
+++ midspace admin helper/Data/Scripts/midspace.adminscripts/UserCommands/CommandSettings.cs
@@ -3,7 +3,6 @@
     using System.Collections.Generic;
     using System.Linq;
     using System.Text;
-    using Sandbox.Common.ObjectBuilders;
     using Sandbox.ModAPI;
     using VRage.Game;
     using VRage.Library.Utils;
